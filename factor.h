b83d925546195dd8a047d4fff47fa70c6c1461d3
creih <iherr.9@gmail.com>
2024-01-25 16:28:09 +0200

    made the first prototype functional on small nums

---
+++ factor.h
@@ -0,0 +1,11 @@
+#ifndef FACTOR_H
+#define FACTOR_H
+#include <stdio.h>
+#include <stdlib.h>
+void factorise(int param);
+int read_fromfile(const char *fil);
+
+
+
+
+#endif
---
+++ nbr_fromfile.c
@@ -0,0 +1,56 @@
+#include "factor.h"
+/**
+ * factorise - this function factorizes the number passed as parameter
+ * @param: this is the parameter to factorise
+ * Return: NULL from my thinking
+ */
+void factorise(int param)
+{
+	int i = 0, j = 0;
+	
+	for (i = 0; i < param; i++)
+	{
+		for (j = 0; j < param; j++)
+		{
+			if (i * j == param)
+			{
+				printf("%d=%d*%d\n", param, j, i);
+				return;
+			}
+		}
+	}
+}
+
+/**
+ * read_fromfile - this function reads nbrs from file line by line
+ * @fil: the file containing the numbers
+ * Return: array of read nbrs
+ */
+int read_fromfile(const char *fil)
+{
+	int umubare, inshuro = 0;
+	FILE *doc = fopen(fil, "r");
+
+	if (!doc)
+	{
+		fprintf(stderr, "Error opening file: %s\n", fil);
+		return (-1);
+	}
+	while (fscanf(doc, "%d", &umubare) == 1)
+	{
+		factorise(umubare);
+		inshuro += 1;
+	}
+
+	fclose(doc);
+	return (inshuro);
+}
+int main(int args, char **argv)
+{
+	int nbr_nbrs = 0;
+	
+	if (args != 2)
+		return (-1);
+	nbr_nbrs = read_fromfile(argv[1]);
+	return (nbr_nbrs);
+}
